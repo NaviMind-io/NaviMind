c8832857e143119220dc374dec2448b3ada9ced1
NaviMind <lomakinvlad@gmail.com>
2026-02-10 16:06:40 +0100

    still a life

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div>OK</div>;
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // При заходе на "/" сразу ведём пользователя на welcome
+    router.replace("/welcome");
+  }, [router]);
+
+  return null; // ничего не рендерим, сразу редирект
 }
